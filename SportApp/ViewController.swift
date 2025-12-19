ccc00ec10659211ead38750a4fda1bb7823c7a68
akose34 <ahmetkose.info@gmail.com>
2025-12-19 10:13:45 +0300

    Initial Commit

---
+++ SportApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SportApp
+//
+//  Created by ahmet köse on 19.12.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
